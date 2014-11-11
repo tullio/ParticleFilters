015543efea8301b4558e4a1d0c6ca5d10b1b8987
tetsu.sato <tetsu.sato@nhn-playart.com>
2014-11-11 19:17:29 +0900

    First commit

---
+++ ParticleFilters/src/com/sofken/pf/Particle.scala
@@ -0,0 +1,8 @@
+package com.sofken.pf
+
+class Particle {
+  def test = {
+    println("Hello, World");
+  }
+
+}
---
+++ ParticleFilters/src/com/sofken/pf/test/ParticleTest.scala
@@ -0,0 +1,12 @@
+package com.sofken.pf.test
+
+import org.junit._
+
+class ParticleTest {
+
+  @Test
+  def test {
+    println("test")
+  }
+  
+}
