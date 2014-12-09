5f75a7dbb2e9a1b57b23a9641a1f605c8d6d118b
tetsu.sato <tetsu.sato@nhn-playart.com>
2014-12-09 14:41:14 +0900

    Second commit

---
+++ src/com/sofken/pf/Particle.scala
@@ -0,0 +1,35 @@
+package com.sofken.pf
+
+
+class Particle[A] {
+  var p = Seq[A]()
+  var dimension:Int = _
+  var initValue:A = _  
+  def this(dimension:Int) = {
+    this()
+    this.dimension = dimension
+    p = p.padTo(dimension, initValue)   
+  }
+    def this(dimension:Int, init:A) = {
+    this()
+    initValue = init
+    this.dimension = dimension
+    p = p.padTo(dimension, initValue)   
+  }
+  def resize(dimension:Int):Int = {
+    return resize(dimension, initValue)
+  }
+   def resize(dimension:Int, init:A):Int = {
+    initValue = init
+    if(dimension < this.dimension) {
+      p = p.padTo(dimension - this.dimension , initValue)
+    } else {
+      p = p.take(dimension)
+      
+    }
+    this.dimension = dimension
+    return this.dimension
+
+  }
+
+}
---
+++ src/com/sofken/pf/test/ParticleTest.scala
@@ -0,0 +1,28 @@
+package com.sofken.pf.test
+import org.junit.Assert.assertThat
+import org.hamcrest.CoreMatchers.is
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.core.IsNull.nullValue
+import com.sofken.pf.Particle
+import org.junit.Test
+
+class ParticleTest {
+
+  @Test
+  def defaultValueTest {
+    var obj = new Particle[Int]()
+    assertThat(obj, notNullValue())
+    assertThat(obj.resize(3), is(3))
+    assertThat(obj.dimension, is(3))
+    val it = obj.p.iterator
+    it.foreach(x => assertThat(x, is(0) ))
+    assertThat(obj.resize(2), is(2))
+    assertThat(obj.dimension, is(2))
+  }
+  @Test
+  def valuedInitializeTest {
+    var obj = new Particle[Double](3)
+    val it = obj.p.iterator
+    it.foreach(x => assertThat(x, is(0D) ))
+  }
+}
